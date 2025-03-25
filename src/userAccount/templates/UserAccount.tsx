ff88cbacc0face0396c784a649244eb5425eaaff
Tibardoe <tibardoebenjamin@gmail.com>
2025-03-25 14:50:55 +0000

    Remove classname from user-account page to layout

--- src/userAccount/templates/UserAccount.tsx
+++ src/userAccount/templates/UserAccount.tsx
@@ -4,12 +4,10 @@ import ProfileDetails from "../organisms/ProfileDetails";
 
 export default function UserAccount() {
   return (
-    <div className="w-[90%] md:w-[80%] mx-auto pt-24 md:pt-32">
-      <div className="flex flex-col gap-10">
-        <ProfileDetails />
-        <Higlight />
-        <ContentArea />
-      </div>
+    <div className="flex flex-col gap-10">
+      <ProfileDetails />
+      <Higlight />
+      <ContentArea />
     </div>
   );
 }
