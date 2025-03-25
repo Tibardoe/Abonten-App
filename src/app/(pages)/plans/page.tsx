73d2d9af126cba9e9140dee1c7efb360de86d6ad
Tibardoe <tibardoebenjamin@gmail.com>
2025-03-25 17:15:04 +0000

    Add screen responsiveness and checkmark to the border of plan container

--- src/app/(pages)/plans/page.tsx
+++ src/app/(pages)/plans/page.tsx
@@ -1,7 +1,11 @@
+"use client";
+
 import PlanContainer from "@/components/molecules/PlanContainer";
-import { Button } from "@/components/ui/button";
+import { useState } from "react";
 
 export default function page() {
+  const [activeSubscription, setActiveSubscription] = useState("Daily");
+
   return (
     <div className="space-y-3">
       <h1 className="font-semibold text-xl md:text-2xl">Change plan</h1>
@@ -10,27 +14,35 @@ export default function page() {
           subscriptionName="Daily"
           subscriptionDetails="Post 2 flyers for a day, unlimited story  post for a day. 24hours after events to post event highlight, post last for a day."
           subscriptionPrice="GHS 5/day"
+          activeSubscription={activeSubscription}
+          setActiveSubscription={setActiveSubscription}
         />
 
         <PlanContainer
           subscriptionName="Weekly"
           subscriptionDetails="Post 5 flyers for a week, unlimited story  post for week. 3days after events to post event highlight, post last for a week."
           subscriptionPrice="GHS 25/day"
+          activeSubscription={activeSubscription}
+          setActiveSubscription={setActiveSubscription}
         />
 
         <PlanContainer
           subscriptionName="Monthly"
           subscriptionDetails="Post 10 flyers for a month, unlimited story  post for a month. 1 week after events to post event highlight, post last for a month."
           subscriptionPrice="GHS 75/day"
+          activeSubscription={activeSubscription}
+          setActiveSubscription={setActiveSubscription}
         />
 
         <PlanContainer
           subscriptionName="Unlimited"
           subscriptionDetails="Post unlimited flyers, unlimited stories. Post highlight of past events anytime. Post last forever."
           subscriptionPrice="GHS 120/day"
+          activeSubscription={activeSubscription}
+          setActiveSubscription={setActiveSubscription}
         />
 
-        <div className="flex gap-5 self-center">
+        <div className="flex gap-5 self-center mb-10">
           <button
             type="button"
             className="font-bold border border-black rounded-md text-lg py-1 px-5"
--- src/components/molecules/PlanContainer.tsx
+++ src/components/molecules/PlanContainer.tsx
@@ -1,26 +1,49 @@
+import { useState } from "react";
+import { cn } from "../lib/utils";
+
 type PlanContainerProp = {
   subscriptionName: string;
   subscriptionDetails: string;
   subscriptionPrice: string;
+  activeSubscription: string;
+  setActiveSubscription: (name: string) => void;
 };
 
 export default function PlanContainer({
   subscriptionName,
   subscriptionDetails,
   subscriptionPrice,
+  activeSubscription,
+  setActiveSubscription,
 }: PlanContainerProp) {
+  const isActive = subscriptionName === activeSubscription;
+
+  const handleClick = () => {
+    setActiveSubscription(subscriptionName);
+  };
+
   return (
     <button
       type="button"
-      className="w-full md:h-36 grid grid-cols-[120px_1fr_auto] gap-5 md:grid-cols-[150px_1fr_auto] border-[4px] md:border-[6px] items-center justify-start border-black"
+      onClick={handleClick}
+      className={cn(
+        "w-full md:h-36 relative flex flex-col md:grid gap-3 md:grid-cols-[150px_1fr_auto] border-[4px] md:gap-5 md:border-[6px] items-center justify-start border-black opacity-50",
+        { "opacity-100": isActive },
+      )}
     >
+      {isActive && (
+        <span className="h-9 w-9 bg-black rounded-full grid place-items-center absolute -left-5 top-1/2 -translate-y-1/2">
+          <div className="border-r-[3px] border-b-[3px] border-white w-[11px] h-5 rotate-45 -translate-y-1" />
+        </span>
+      )}
+
       <span className="w-full h-full p-5 grid place-items-center bg-black bg-opacity-5">
         <h2 className="font-bold md:text-xl"> {subscriptionName}</h2>
       </span>
-      <span className="w-full text-sm md:text-lg text-start py-5">
+      <span className="text-sm md:text-lg md:text-start px-5">
         <p>{subscriptionDetails}</p>
       </span>
-      <span className="w-full p-5 grid place-items-center md:text-lg ">
+      <span className="p-5 pl-0 grid place-items-center md:text-lg ">
         <p>{subscriptionPrice}</p>
       </span>
     </button>
