ef8c36ba8430ce82228512a85a008c8d48143f97
Tibardoe <tibardoebenjamin@gmail.com>
2025-03-26 17:00:51 +0000

    Add mobile money  method UI

---
+++ src/utils/networkProviderData.ts
@@ -0,0 +1,6 @@
+export const networks = [
+  { logo: "/assets/images/atLogo.png", network: "AT Money" },
+  { logo: "/assets/images/gmoneyLogo.png", network: "G-Money" },
+  { logo: "/assets/images/mtnmomoLogo.png", network: "MTN Mobile Money" },
+  { logo: "/assets/images/telecelcashLogo.png", network: "Telecel Cash" },
+];
--- src/wallet/organisms/AddMomoWallet.tsx
+++ src/wallet/organisms/AddMomoWallet.tsx
@@ -1,7 +1,8 @@
-import { useForm } from "react-hook-form";
-
 import { Button } from "@/components/ui/button";
+import { networks } from "@/utils/networkProviderData";
 import Image from "next/image";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
 
 type PopupCloseProp = {
   onclick: () => void;
@@ -11,6 +12,12 @@ export default function AddMomoWallet({ onclick }: PopupCloseProp) {
   const form = useForm();
 
   const { register } = form;
+
+  const [showDropdown, setShowDropdown] = useState(false);
+
+  const handleDropdown = () => {
+    setShowDropdown((prevState) => !prevState);
+  };
 
   return (
     // biome-ignore lint/a11y/useKeyWithClickEvents: <explanation>
@@ -29,6 +36,26 @@ export default function AddMomoWallet({ onclick }: PopupCloseProp) {
             height={25}
           />
         </button>
+      </div>
+
+      {/* Mobile header */}
+      <div className="flex flex-col gap-2 md:hidden pb-10">
+        <div className="flex items-center w-full">
+          <button type="button" onClick={onclick}>
+            <Image
+              src="/assets/images/arrowLeft.svg"
+              alt="Close"
+              width={30}
+              height={30}
+              className="self-start"
+            />
+          </button>
+          <h1 className="font-bold text-xl m-auto">Add Mobile Money Wallet</h1>
+        </div>
+
+        <p className="text-center text-sm">
+          Pay with your mobile money wallet from any provider
+        </p>
       </div>
 
       <form className="flex flex-col gap-5">
@@ -51,19 +78,76 @@ export default function AddMomoWallet({ onclick }: PopupCloseProp) {
             Select Mobile Money Network
           </label>
 
-          <div className="border border-black rounded-md border-opacity-30 px-4 py-2 bg-transparent">
-            <select
-              name="mobile money network"
-              className="bg-white outline-none w-full"
-            >
-              <option value="AT Money">Select mobile network</option>
+          <button
+            type="button"
+            onClick={handleDropdown}
+            className="border border-black rounded-md border-opacity-30 px-4 py-2 bg-transparent flex justify-between items-center"
+          >
+            <p>Select mobile network</p>
+            <Image
+              src="/assets/images/arrowDown.svg"
+              alt="Dropdown icon"
+              width={30}
+              height={30}
+            />
+          </button>
 
-              <option value="AT Money">AT Money</option>
-            </select>
-          </div>
+          {showDropdown && (
+            <>
+              {/* for mobile */}
+              {/* biome-ignore lint/a11y/useKeyWithClickEvents: <explanation> */}
+              <div
+                onClick={onclick}
+                className="flex items-end md:hidden fixed top-0 left-0 w-full bg-black bg-opacity-70 h-dvh"
+              >
+                {/* biome-ignore lint/a11y/useKeyWithClickEvents: <explanation> */}
+                <ul
+                  onClick={(e) => e.stopPropagation()}
+                  className="w-full bg-white rounded-t-xl p-4"
+                >
+                  {networks.map((network) => (
+                    <button
+                      type="button"
+                      key={network.network}
+                      className="flex w-full gap-2 justify-start items-center border-b border-black border-opacity-30 py-3"
+                    >
+                      <Image
+                        src={network.logo}
+                        alt={network.network}
+                        width={50}
+                        height={50}
+                      />
+                      {network.network}
+                    </button>
+                  ))}
+                </ul>
+              </div>
+
+              {/* For desktop */}
+              <ul className="hidden md:flex flex-col gap-3 shadow-xl h-64 p-3 overflow-y-scroll rounded-xl">
+                {networks.map((network) => (
+                  <button
+                    type="button"
+                    key={network.network}
+                    className="flex gap-2 justify-start items-center border-b border-black border-opacity-30 py-3"
+                  >
+                    <Image
+                      src={network.logo}
+                      alt={network.network}
+                      width={50}
+                      height={50}
+                    />
+                    {network.network}
+                  </button>
+                ))}
+              </ul>
+            </>
+          )}
         </div>
 
-        <Button className="font-semibold self-end">Save This Wallet</Button>
+        <Button className="font-semibold md:self-end rounded-full md:rounded-md py-6 text-lg md:text-sm">
+          Save This Wallet
+        </Button>
       </form>
     </div>
   );
