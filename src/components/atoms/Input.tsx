11b6e8ccbe04d0d986656b27ec9642c3db659c9d
Tibardoe <tibardoebenjamin@gmail.com>
2025-04-02 00:33:28 +0100

    Add forward ref to Input component in edit profile page

--- src/components/atoms/Input.tsx
+++ src/components/atoms/Input.tsx
@@ -1,52 +1,70 @@
 "use client";
 
-import { useRef, useState } from "react";
+import { forwardRef, useRef, useState } from "react";
 
 type inputProp = {
   title: string;
   inputPlaceholder: string;
-};
+} & React.InputHTMLAttributes<HTMLInputElement | HTMLTextAreaElement>;
 
-export default function Input({ title, inputPlaceholder }: inputProp) {
-  const [inputFieldDisabled, setInputFieldDisabled] = useState(true);
+const Input = forwardRef<HTMLInputElement | HTMLTextAreaElement, inputProp>(
+  ({ title, inputPlaceholder, ...props }, ref) => {
+    const [inputFieldDisabled, setInputFieldDisabled] = useState(true);
 
-  const inputRef = useRef<HTMLInputElement | null>(null);
+    const inputRef = useRef<HTMLInputElement | null>(null);
 
-  const handleClick = () => {
-    setInputFieldDisabled((prevState) => !prevState);
-    setTimeout(() => inputRef.current?.focus(), 0);
-  };
+    const handleClick = () => {
+      setInputFieldDisabled((prevState) => !prevState);
+      setTimeout(() => inputRef.current?.focus(), 0);
+    };
 
-  return (
-    <div className="space-y-2">
-      <label htmlFor={title} className="font-bold md:text-lg">
-        {title}
-      </label>
+    return (
+      <div className="space-y-2">
+        <label htmlFor={title} className="font-bold md:text-lg">
+          {title}
+        </label>
 
-      <div className="w-full flex justify-between items-center gap-5 p-3 rounded-md border border-black border-opacity-30">
-        {title === "Bio" ? (
-          <textarea
-            className="bg-transparent outline-none text-md md:textlg lg:text-xl flex-1"
-            placeholder={inputPlaceholder}
-          />
-        ) : (
-          <input
-            type="text"
-            ref={inputRef}
-            placeholder={inputPlaceholder}
-            disabled={title === "Bio" ? false : inputFieldDisabled}
-            className="bg-transparent outline-none text-md md:textlg lg:text-xl flex-1"
-          />
-        )}
+        <div className="w-full flex justify-between items-center gap-5 p-3 rounded-md border border-black border-opacity-30">
+          {title === "Bio" ? (
+            <textarea
+              ref={ref as React.Ref<HTMLTextAreaElement>}
+              className="bg-transparent outline-none text-md md:textlg lg:text-xl flex-1"
+              placeholder={inputPlaceholder}
+              {...props}
+            />
+          ) : (
+            <input
+              type="text"
+              ref={(e) => {
+                inputRef.current = e;
+                if (typeof ref === "function") {
+                  ref(e);
+                } else if (ref) {
+                  (
+                    ref as React.MutableRefObject<HTMLInputElement | null>
+                  ).current = e;
+                }
+              }}
+              placeholder={inputPlaceholder}
+              disabled={title === "Bio" ? false : inputFieldDisabled}
+              className="bg-transparent outline-none text-md md:textlg lg:text-xl flex-1"
+              {...props}
+            />
+          )}
 
-        <button
-          type="button"
-          onClick={handleClick}
-          className={title === "Bio" ? "hidden" : "flex font-bold"}
-        >
-          Edit
-        </button>
+          <button
+            type="button"
+            onClick={handleClick}
+            className={title === "Bio" ? "hidden" : "flex font-bold"}
+          >
+            Edit
+          </button>
+        </div>
       </div>
-    </div>
-  );
-}
+    );
+  },
+);
+
+Input.displayName = "Input";
+
+export default Input;
