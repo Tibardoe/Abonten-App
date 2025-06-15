5b4d2a51d4a806c227080cf537ad65d6fb2fbef3
Tibardoe <tibardoebenjamin@gmail.com>
2025-06-15 18:55:29 +0100

    Update edit profile form response

--- src/components/organisms/EditProfileInputFields.tsx
+++ src/components/organisms/EditProfileInputFields.tsx
@@ -2,8 +2,11 @@
 
 import { updateUserDetails } from "@/actions/updateUserDetails";
 import type { UserDetailsFormType } from "@/types/userProfileType";
+import { useMutation } from "@tanstack/react-query";
+import { useState } from "react";
 import { useForm } from "react-hook-form";
 import Input from "../atoms/Input";
+import Notification from "../atoms/Notification";
 import { Button } from "../ui/button";
 
 type InitialDataProps = {
@@ -25,29 +28,59 @@ export default function EditProfileInputFields({
 
   const { register, handleSubmit } = form;
 
+  const [notification, setNotification] = useState<string | null>(null);
+
+  const { mutate, isPending } = useMutation({
+    mutationFn: updateUserDetails,
+    onSuccess: (profileData) => {
+      setNotification(profileData?.message || "Profile updated successfully.");
+    },
+    onError: (error) => {
+      setNotification(error?.message || "Something went wrong.");
+    },
+    onSettled: () => {
+      setTimeout(() => {
+        setNotification(null);
+      }, 3000);
+    },
+  });
+
   const onSubmit = (data: UserDetailsFormType) => {
-    updateUserDetails(data);
+    mutate(data);
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-5">
-      <Input
-        title="Username"
-        inputPlaceholder="Username"
-        {...register("username")}
-      />
+    <>
+      <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-5">
+        <Input
+          title="Username"
+          inputPlaceholder="Username"
+          {...register("username")}
+        />
 
-      <Input title="Name" inputPlaceholder="Name" {...register("full_name")} />
+        <Input
+          title="Name"
+          inputPlaceholder="Name"
+          {...register("full_name")}
+        />
 
-      <Input
-        title="Website"
-        inputPlaceholder="Website"
-        {...register("website")}
-      />
+        <Input
+          title="Website"
+          inputPlaceholder="Website"
+          {...register("website")}
+        />
 
-      <Input title="Bio" inputPlaceholder="Bio" {...register("bio")} />
+        <Input title="Bio" inputPlaceholder="Bio" {...register("bio")} />
 
-      <Button className="self-end font-bold">Submit</Button>
-    </form>
+        <Button
+          className="self-end font-bold mb-5 md:mb-0"
+          disabled={isPending}
+        >
+          {isPending ? "Submitting..." : "Submit"}
+        </Button>
+      </form>
+
+      <Notification notification={notification} />
+    </>
   );
 }
