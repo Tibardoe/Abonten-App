c5a30b9d6b84539b7f6b521094f8eec36c2e2063
Tibardoe <tibardoebenjamin@gmail.com>
2025-06-20 17:52:54 +0100

    Update ui for highlight modal

--- src/components/organisms/HighlightModal.tsx
+++ src/components/organisms/HighlightModal.tsx
@@ -603,7 +603,7 @@ export default function HighlightModal({
         <div className="w-full h-full flex flex-col items-center justify-center relative">
           {/* Conditional rendering for ImageCropper or main steps */}
           {isCropping && imageToCrop ? (
-            <div className="w-full h-[80%] md:w-[60%] bg-white overflow-y-scroll md:overflow-y-auto">
+            <div className="w-full md:h-[90%] md:w-[60%] bg-white overflow-y-scroll md:overflow-y-auto">
               <ImageCropper
                 imagePreview={imageToCrop}
                 handleCropped={handleCropped}
